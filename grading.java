1e52dce6c433aca978ae37e17f6cd84d98901b8c
Akash <akash16.04.2005@gmail.com>
2024-08-01 22:12:12 +0530

    Update grading.java

--- grading.java
+++ grading.java
@@ -1,25 +1,27 @@
-
 import java.util.Scanner;
 
 public class grade {
-
     public static void main(String[] args) {
-        String grade;
-        Scanner sc = new Scanner(System.in);
-        int mark = sc.nextInt();
-        if (mark >= 90 && mark <= 100) {
-            grade = "A";
-        } else if (mark < 90 && mark >= 80) {
-            grade = "B";
-        } else if (mark < 80 && mark >= 70) {
-            grade = "C";
-        } else if (mark < 70 && mark >= 60) {
-            grade = "D";
-        } else if (mark > 100 || mark < 0) {
-            grade = "enter valid marks";
-        } else {
-            grade = "F";
+        for (int i = 0; i < 10; i++) {
+            String grade;
+            int[] mark=new int[11];
+            Scanner sc = new Scanner(System.in);
+            mark[i] = sc.nextInt();
+            if (mark[i] >= 90 && mark[i] <= 100) {
+                grade = "A";
+            } else if (mark[i] < 90 && mark[i] >= 80) {
+                grade = "B";
+            } else if (mark[i] < 80 && mark[i] >= 70) {
+                grade = "C";
+            } else if (mark[i] < 70 && mark[i] >= 60) {
+                grade = "D";
+            } else if (mark[i] > 100 || mark[i] < 0) {
+                grade = "enter valid marks";
+            } else {
+                grade = "F";
+            }
+            System.out.println(grade);
         }
-        System.out.println(grade);
     }
 }
+
